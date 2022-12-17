a3770bfd39de8b7f1a1ecfd7b552791c33806cda
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2022-12-17 08:49:10 -0500

    intento de cambiar background

--- login.php
+++ login.php
@@ -14,15 +14,15 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400&display=swap" rel="stylesheet">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
-    integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+    integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous"> -->
 
     <link rel="stylesheet" href="css/estilos.css">
 </head>
 
 <body>
 
-    <img class="nave" src="img/nave.png">
+    <!-- <img class="nave" src="img/banner.jpg"> -->
     <div class="container">
         <div class="img">
 
